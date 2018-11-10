ae9181abf001ce1ec40889e14f608392e5bbb4cf
TatianaArslanova <tashilova@narod.ru>
2018-11-10 18:38:11 +0500

    add pull-to-refresh(kotlin)

--- app/src/kotlin/java/com/example/ama/viewer/presentation/main/MainFragment.kt
+++ app/src/kotlin/java/com/example/ama/viewer/presentation/main/MainFragment.kt
@@ -1,9 +1,7 @@
 package com.example.ama.viewer.presentation.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import com.example.ama.viewer.R
 import com.example.ama.viewer.data.repo.DataRepositoryImpl
@@ -13,6 +11,7 @@ import com.example.ama.viewer.presentation.main.mvp.base.MainView
 import com.hannesdorfmann.mosby3.mvp.viewstate.lce.LceViewState
 import com.hannesdorfmann.mosby3.mvp.viewstate.lce.MvpLceViewStateFragment
 import com.hannesdorfmann.mosby3.mvp.viewstate.lce.data.RetainingLceViewState
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : MvpLceViewStateFragment<TextView, String, MainView, MainPresenter>(), MainView {
 
@@ -22,11 +21,29 @@ class MainFragment : MvpLceViewStateFragment<TextView, String, MainView, MainPre
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        retainInstance=true
+        retainInstance = true
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        srl_layout.setOnRefreshListener { loadData(true) }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.mi_refresh) {
+            loadData(false)
+            return true
+        }
+        return false
     }
 
     override fun loadData(pullToRefresh: Boolean) {
@@ -48,6 +65,12 @@ class MainFragment : MvpLceViewStateFragment<TextView, String, MainView, MainPre
     override fun getData() = contentView.text.toString()
 
     override fun getErrorMessage(e: Throwable?, pullToRefresh: Boolean): String {
+        if (pullToRefresh) srl_layout.isRefreshing = false
         return resources.getString(R.string.unknown_error)
+    }
+
+    override fun showContent() {
+        super.showContent()
+        srl_layout.isRefreshing = false
     }
 }
