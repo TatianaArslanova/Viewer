f828e76a16e8d93032c6ebe0c07d4847f0b92115
TatianaArslanova <tashilova@narod.ru>
2018-11-27 13:47:04 +0500

    add stetho, create RequestHeadersInterceptor

--- app/src/java/java/com/example/ama/viewer/ViewerApp.java
+++ app/src/java/java/com/example/ama/viewer/ViewerApp.java
@@ -1,9 +1,15 @@
 package com.example.ama.viewer;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.example.ama.viewer.data.api.GithubApi;
+import com.example.ama.viewer.data.interceptor.RequestHeadersInterceptor;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,16 +23,41 @@ public class ViewerApp extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        initStetho();
         initApi();
+    }
+
+    private void initStetho() {
+        Stetho.Initializer initializer = Stetho.newInitializerBuilder(this)
+                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                .build();
+        Stetho.initialize(initializer);
     }
 
     private void initApi() {
         githubApi = new Retrofit.Builder()
+                .client(buildClient())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build()
                 .create(GithubApi.class);
+    }
+
+    private OkHttpClient buildClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new RequestHeadersInterceptor(getPackageInfo()))
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+    }
+
+    private PackageInfo getPackageInfo() {
+        try {
+            return getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static ViewerApp getInstance() {
---
+++ app/src/java/java/com/example/ama/viewer/data/interceptor/RequestHeadersInterceptor.java
@@ -0,0 +1,40 @@
+package com.example.ama.viewer.data.interceptor;
+
+import android.content.pm.PackageInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestHeadersInterceptor implements Interceptor {
+    private static final String UNKNOWN = "UNKNOWN";
+    private static final String MEDIA_TYPE = "application/vnd.github.v3+json";
+
+    @Nullable
+    private PackageInfo packageInfo;
+
+    public RequestHeadersInterceptor(@Nullable PackageInfo packageInfo) {
+        this.packageInfo = packageInfo;
+    }
+
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        Request updatedRequest = request.newBuilder()
+                .addHeader("User-Agent", buildAppInfo())
+                .addHeader("Accept", MEDIA_TYPE)
+                .build();
+        return chain.proceed(updatedRequest);
+    }
+
+    private String buildAppInfo() {
+        if (packageInfo != null) {
+            return packageInfo.applicationInfo.name + " " + packageInfo.versionName;
+        }
+        return UNKNOWN;
+    }
+}
